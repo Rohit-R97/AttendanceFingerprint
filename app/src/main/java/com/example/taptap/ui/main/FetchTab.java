62c9e916492f9be8df7a30e543d7676ddae1436d
PSmayank21 <mayankpsindhvad@gmail.com>
2020-03-03 17:50:26 +0530

    Tab Layouts Created

---
+++ app/src/main/java/com/example/taptap/ui/main/FetchTab.java
@@ -0,0 +1,16 @@
+package com.example.taptap.ui.main;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.taptap.R;
+
+public class FetchTab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_fetch);
+    }
+}
