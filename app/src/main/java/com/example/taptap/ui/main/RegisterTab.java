bd6af51cfb25bf9573caaff94aa567a6c46c8c55
PSmayank21 <mayankpsindhvad@gmail.com>
2020-03-04 11:24:20 +0530

    JSGFPLib added

--- app/src/main/java/com/example/taptap/ui/main/RegisterTab.java
+++ app/src/main/java/com/example/taptap/ui/main/RegisterTab.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import SecuGen.FDxSDKPro.*;
 import com.example.taptap.R;
 
 
 public class RegisterTab extends AppCompatActivity {
+
+    private JSGFPLib sgfplib;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
